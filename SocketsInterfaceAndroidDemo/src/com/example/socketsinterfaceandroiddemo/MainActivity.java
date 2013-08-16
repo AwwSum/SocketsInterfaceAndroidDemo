20d8ff18c54168f47c88a2d898614744957121a7
Joseph Leclerc <Joezus2434@aol.com>
2013-08-16 12:38:44 -0400

    Completely working, NAT-traversing, server-connecting app able to receive messages from other senders.

--- SocketsInterfaceAndroidDemo/src/com/example/socketsinterfaceandroiddemo/MainActivity.java
+++ SocketsInterfaceAndroidDemo/src/com/example/socketsinterfaceandroiddemo/MainActivity.java
@@ -1,6 +1,14 @@
 package com.example.socketsinterfaceandroiddemo;
 
+import umass.socketsInterface.client.*; //the sockets interface library
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
@@ -13,6 +21,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*	Allow network calls to be done on the main thread.
+         * 	source: http://stackoverflow.com/questions/6343166/android-os-networkonmainthreadexception
+         * 	By default android throws an exception when potentially long-running I/O is done on the main thread.
+         * 		However, in this particular instance, that is literally the only purpose of this app existing,
+         * 		so it's okay.
+         */
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
     }
 
     @Override
@@ -23,10 +39,15 @@ public class MainActivity extends Activity {
     }
     
     //make sure that the button works, and that text can be inserted into the text view.
-    public void testButton(View view){
+    public void testButton(View view) throws IOException{
     	//Intent testButtonIntent = new Intent(this, MainActivity.class);
     	TextView helloWorldTextView = (TextView)findViewById(R.id.textView1);
-    	helloWorldTextView.setText("The button works!");
+    	//helloWorldTextView.setText("The button works!");
+    	Client myClient = new Client("184.72.220.54", 9119); //184.72.220.54 is the aws elastic IP of the server.
+    	InputStream inFromWeb = myClient.getInputStream();
+    	BufferedReader reader = new BufferedReader(new InputStreamReader(inFromWeb));
+    	String message = reader.readLine(); //throws the IOException
+    	helloWorldTextView.setText(message);
     }
     
 }
--- SocketsInterfaceAndroidDemo/src/com/example/socketsinterfaceandroiddemo/UpdateTextActivity.java
+++
@@ -1,7 +0,0 @@
-package com.example.socketsinterfaceandroiddemo;
-
-import android.app.Activity;
-
-public class UpdateTextActivity extends Activity {
-
-}
